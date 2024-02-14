4b2936ed6d22d94a7ce681327a0a900450687a5a
Napawan Nakasuwan <97944321+ampamp1a@users.noreply.github.com>
2024-02-14 15:26:27 +0700

    Update modbus_RTU.ino

--- Lab5/modbus_RTU/modbus_RTU.ino
+++ Lab5/modbus_RTU/modbus_RTU.ino
@@ -5,17 +5,12 @@ int8_t indexMax = 0;
 int8_t m_error = 0;
 int8_t ready = 0;
 unsigned long prev_t;
-uint8_t packet[6];
-                                            /* packet with CRC16 */
-                                          /*CRC(16) Calculator  https://crccalc.com/ */
-          //          address   function code   data address 2 byte   data length 2 byte     CRC2 byte
-uint8_t packet1[8]; //= {  0x01,       0x03,           0x00,0x00,           0x00,0x02,          0xC4,0x0B}; //V Phase A (a-n)
-//uint8_t packet2[8] = {  0x01,       0x03,           0x00,0x32,           0x00,0x02,          0x65,0xC4}; //Frequency
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-String Ex_String_Read;
-unsigned long address,dataAdd1,dataAdd2,dataLen1,dataLen2,crc1,crc2;
+uint8_t packet[6]; // for recieve from user and sent to CRC Algorithm
+uint8_t packet1[8]; // for sent packet to power meter
+String Ex_String_Read; // keep data from serial monitor
+unsigned long address,dataAdd1,dataAdd2,dataLen1,dataLen2,crc1,crc2; // data in packet
 uint16_t crc;
-int Result;
+int Result; // Result to show for user
 void setup() {
   Serial.begin(115200);
   pinMode(dirpin, OUTPUT);
@@ -30,7 +25,8 @@ void setup() {
 void get_value(String value) {
 
   if (sscanf(value.c_str(), "address: %x data address: %x %x data length: %x %x",&address,&dataAdd1,&dataAdd2,&dataLen1,&dataLen2) == 5) {
-    //address = address 
+    //seperate to keep data for packet
+    // show value each data for keep packet
     Serial.print("Address: ");
     Serial.println(address,HEX);
     Serial.print("Data address: ");
@@ -38,16 +34,18 @@ void get_value(String value) {
     Serial.println(dataAdd2,HEX);
     Serial.print("Data length: ");
     Serial.print(dataLen1,HEX);
-    Serial.println(dataLen2,HEX);    
+    Serial.println(dataLen2,HEX);   
+    // keep data in packet to sent for calculate crc
         packet[0] = address & 0xFF; //address
         packet[1] = 0x03; // Function code
         packet[2] = dataAdd1 & 0xFF; //dataAdd1
         packet[3] = dataAdd2 & 0xFF; //dataAdd2
         packet[4] = dataLen1 & 0xFF; //dataLen1
         packet[5] = dataLen2 & 0xFF; //dataLen2
-    crc = MODBUS_CRC16_v1(packet,6);
-    crc1 = crc & 0xFF;
-    crc2 = (crc >> 8) & 0xFF;
+    crc = MODBUS_CRC16_v1(packet,6); // sent packet for calculate crc 
+    crc1 = crc & 0xFF;  // LSB
+    crc2 = (crc >> 8) & 0xFF; // MSB
+    // keep data in packet to sent for Power meter
         packet1[0] = packet[0]; //address
         packet1[1] = 0x03; // Function code
         packet1[2] = packet[2]; //dataAdd1
@@ -56,16 +54,12 @@ void get_value(String value) {
         packet1[5] = packet[5]; //dataLen2
         packet1[6] = crc1;
         packet1[7] = crc2;
-    //                                           /* packet with CRC16 */
-    //                                         /*CRC(16) Calculator  https://crccalc.com/ */
-    //         //          address   function code   data address 2 byte   data length 2 byte     CRC2 byte
-    //  uint8_t packet1[8] = {  packet[0],       0x03,           packet[2],packet[3],           packet[4],packet[5],          crc1,crc2}; //V Phase A (a-n)
+
     ready = 1;
-  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
   } else {
     Serial.println("Invalid Format");
   }
-
 
 }
 
@@ -95,12 +89,6 @@ static uint16_t MODBUS_CRC16_v1( const unsigned char *buf, unsigned int len )
 
 	return crc;
 }
-byte decToBcd(byte val) {
-  return ((val / 10) << 4) | (val % 10);
-}
-byte bcdToDec(byte b) {
-  return ((b >> 4) * 10) + (b & 0x0F);
-}
 
 void loop() {
 
@@ -108,7 +96,7 @@ while(Serial.available()) {  // วนรับค่าหากด้วย wh
     Ex_String_Read = Serial.readString();  // ใช้คำสั้ง Serial.readString() สำหรับเก็บค่า String ไว้ในตัวแปร Ex_String_Read
     Serial.print("Ex_String_Read = ");
     Serial.println(Ex_String_Read);  // แสดงผลออกทาง Serial
-    get_value(Ex_String_Read);
+    get_value(Ex_String_Read); // for call function to prepare packet to sent power meter
 
      if (ready = 1){
         for (int i = 0; i < sizeof(packet1) / sizeof(packet1[0]); i++) {
@@ -143,9 +131,7 @@ while(Serial.available()) {  // วนรับค่าหากด้วย wh
     }
 }
 
-
 }
-
 
 void Data_recv(uint8_t SS) {
   unsigned long prev_t = millis();  //resopnse check, time is over 20 sec set error code = 1 (Timeout Error code) 
